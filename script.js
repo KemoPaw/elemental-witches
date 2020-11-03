a27282c4c630cc0356197aa385625094696c1a24
Kathy Chan <kathykemopaw@gmail.com>
2020-11-03 15:23:28 -0800

    Add score and collision game-over

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var witch = document.getElementById("witch");
 var monster = document.getElementById("monster");
-// var counter = 0;
+var counter = 0;
+
 function jump() {
-    if (witch.classList == "animate") { return }
+    if (witch.classList === "animate") { return }
     witch.classList.add("animate");
     setTimeout(function () {
         witch.classList.remove("animate");
-    }, 300);
+    }, 500);
 }
+
+let checkHit = setInterval(function () {
+    let witchTop = parseInt(window.getComputedStyle(witch).getPropertyValue("top"));
+    let monsterLeft = parseInt(window.getComputedStyle(monster).getPropertyValue("left"));
+    if (monsterLeft < 100 && monsterLeft > -100 && witchTop >= 400){
+        monster.style.animation = "none";
+        alert("Game Over. score: " + Math.floor(counter / 100));
+        counter = 0;
+        monster.style.animation = "block 1s infinite linear";
+    } else {
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter / 100);
+    }
+}, 20);
